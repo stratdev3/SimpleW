849089bd5f74587310c0fe147730453fb7c86f53
Christophe CHATEAU <c.chateau@stratdev.fr>
2025-02-11 15:33:02 +0100

    chore: rename MakeResponse(streamingContextObject) to MakeContextResponse(streamingContextObject)

--- src/SimpleW/SimpleW/Controller.cs
+++ src/SimpleW/SimpleW/Controller.cs
@@ -291,20 +291,6 @@ namespace SimpleW {
         /// Make Response from object with JsonSerializerSettings.Context.streamingContextObject StreamingContextStates.Other
         /// </summary>
         /// <param name="content">The object Content.</param>
-        /// <param name="streamingContextObject">The JsonSerializerSettings.StreamingContext.AdditionnalObject (default is null)</param>
-        /// <param name="contentType">The contentType. (default is "text/plain; charset=UTF-8")</param>
-        public HttpResponse MakeResponse(object content, object? streamingContextObject = null, string contentType = "application/json; charset=UTF-8") {
-            return MakeResponse(content, 
-                                new JsonSerializerSettings {
-                                    Context = new StreamingContext(StreamingContextStates.Other, streamingContextObject)
-                                },
-                                contentType);
-        }
-
-        /// <summary>
-        /// Make Response from object with JsonSerializerSettings.Context.streamingContextObject StreamingContextStates.Other
-        /// </summary>
-        /// <param name="content">The object Content.</param>
         /// <param name="settings">The JsonSerializerSettings settings (default is null)</param>
         /// <param name="contentType">The contentType. (default is "text/plain; charset=UTF-8")</param>
         public HttpResponse MakeResponse(object content, JsonSerializerSettings settings = null, string contentType = "application/json; charset=UTF-8") {
@@ -318,6 +304,34 @@ namespace SimpleW {
 
             Response.SetBody(JsonConvert.SerializeObject(content, settings));
             return Response;
+        }
+
+        /// <summary>
+        /// Make Response from object with JsonSerializerSettings.Context.streamingContextObject StreamingContextStates.Other
+        /// <example>
+        /// Example :
+        ///     - in the controller, call with MakeContextResponse(data, "secure");
+        ///     - in the data class, add a OnSerializedMethod() with [OnSerializing] attribute
+        /// <code>
+        /// [OnSerializing]
+        /// internal void OnSerializedMethod(StreamingContext context) {
+        ///    if (context.Context is string ctx &amp;&amp; == "secure") {
+        ///         name = Guid.Empty;
+        ///    }
+        /// }
+        /// </code>
+        /// And so the property "name" of the serialized data instance will be empty (anonymized)
+        /// </example>
+        /// </summary>
+        /// <param name="content">The object Content.</param>
+        /// <param name="streamingContextObject">The JsonSerializerSettings.StreamingContext.AdditionnalObject</param>
+        /// <param name="contentType">The contentType. (default is "text/plain; charset=UTF-8")</param>
+        public HttpResponse MakeContextResponse(object content, object streamingContextObject, string contentType = "application/json; charset=UTF-8") {
+            return MakeResponse(content,
+                                new JsonSerializerSettings {
+                                    Context = new StreamingContext(StreamingContextStates.Other, streamingContextObject)
+                                },
+                                contentType);
         }
 
         /// <summary>
