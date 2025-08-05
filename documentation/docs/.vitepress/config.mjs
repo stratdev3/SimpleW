ccab7b47d1254ad1cf4e4add29ecaa815c35ade9
Christophe CHATEAU <c.chateau@stratdev.fr>
2025-08-05 16:03:49 +0200

    ux/ui(documentation): dark mode as default

--- documentation/docs/.vitepress/config.mjs
+++ documentation/docs/.vitepress/config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from 'vitepress'
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
+    appearance: 'dark',
     base: '/SimpleW/',
     title: 'SimpleW',
     titleTemplate: 'SimpleW',
