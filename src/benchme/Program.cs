6727222ac92d0acb272901f56d06aac4adfaa7f7
Christophe CHATEAU <c.chateau@stratdev.fr>
2025-10-08 21:54:51 +0200

    chore(benchme): add new projet for bechmark purpose

---
+++ src/benchme/Program.cs
@@ -0,0 +1,45 @@
+﻿using SimpleW;
+using SimpleW.Newtonsoft;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+
+
+namespace benchme {
+
+    /// <summary>
+    /// Benchmark Program
+    /// $ dotnet run -c Release benchme.csproj
+    /// </summary>
+    internal class Program {
+
+        /// <summary>
+        /// EntryPoint
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args) {
+
+            var summary = BenchmarkRunner.Run<SimpleWBenchmark>();
+            Console.WriteLine("Done!");
+
+        }
+
+    }
+
+    /// <summary>
+    /// SimpleW Benchmark
+    /// </summary>
+    public class SimpleWBenchmark {
+
+        [Benchmark]
+        public void Current() {
+
+        }
+
+        [Benchmark]
+        public void Optimize() {
+
+        }
+
+    }
+
+}
