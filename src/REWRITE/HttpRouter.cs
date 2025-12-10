e58be8d7ca7a9fd8ecc7df42cf7c2827714c9eae
Christophe CHATEAU <c.chateau@stratdev.fr>
2025-12-10 01:37:35 +0100

    refactor(REWRITE): split file HttpRouter into HttpHandler

--- src/REWRITE/HttpRouter.cs
+++ src/REWRITE/HttpRouter.cs
@@ -1,35 +1,6 @@
 ﻿namespace SimpleW {
 
     /// <summary>
-    /// Delegate for handler that send the response on their own (ValueTask)
-    /// </summary>
-    /// <param name="session"></param>
-    /// <returns></returns>
-    public delegate ValueTask HttpHandlerVoid(HttpSession session);
-
-    /// <summary>
-    /// Delegate for async handler that return a result ValueTask<object?>
-    /// </summary>
-    /// <param name="session"></param>
-    /// <returns></returns>
-    public delegate ValueTask<object?> HttpHandlerAsyncReturn(HttpSession session);
-
-    /// <summary>
-    /// Delegate for sync handler that return a result object?
-    /// </summary>
-    /// <param name="session"></param>
-    /// <returns></returns>
-    public delegate object? HttpHandlerSyncResult(HttpSession session);
-
-    /// <summary>
-    /// Delete for Middleware
-    /// </summary>
-    /// <param name="session"></param>
-    /// <param name="next"></param>
-    /// <returns></returns>
-    public delegate ValueTask HttpMiddleware(HttpSession session, Func<ValueTask> next);
-
-    /// <summary>
     /// HttpRouter
     /// </summary>
     public sealed class HttpRouter {
