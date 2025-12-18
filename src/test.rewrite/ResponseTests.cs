7a9ab81cdd0e36ab040402fb0d1962d0efbaaeca
Christophe CHATEAU <c.chateau@stratdev.fr>
2025-12-18 02:02:57 +0100

    feature(REWRITE): add tests for REWRITE (#144)

---
+++ src/test.rewrite/ResponseTests.cs
@@ -0,0 +1,207 @@
+﻿using System.Net;
+using System.Text.Json;
+using NFluent;
+using SimpleW;
+using Xunit;
+
+
+namespace test {
+
+    /// <summary>
+    /// Tests for response
+    /// </summary>
+    public class ResponseTests {
+
+        [Fact]
+        public async Task Response_200_HelloWorld_Raw() {
+
+            // server
+            var server = new SimpleWServer(IPAddress.Loopback, PortManager.GetFreePort());
+            server.MapGet("/", (HttpSession session) => {
+                return session.Response.Text("Hello World !");
+            });
+            await server.StartAsync();
+
+            // client
+            var client = new HttpClient();
+            var response = await client.GetAsync($"http://{server.Address}:{server.Port}/");
+            var content = await response.Content.ReadAsStringAsync();
+
+            // asserts
+            Check.That(response.StatusCode).IsEqualTo(HttpStatusCode.OK);
+            Check.That(content).IsEqualTo("Hello World !");
+
+            // dispose
+            await server.StopAsync();
+            PortManager.ReleasePort(server.Port);
+        }
+
+        [Fact]
+        public async Task Response_200_HelloWorld_Json() {
+
+            // server
+            var server = new SimpleWServer(IPAddress.Loopback, PortManager.GetFreePort());
+            server.MapGet("/", () => {
+                return new { message = "Hello World !" };
+            });
+            await server.StartAsync();
+
+            // client
+            var client = new HttpClient();
+            var response = await client.GetAsync($"http://{server.Address}:{server.Port}/");
+            var content = await response.Content.ReadAsStringAsync();
+
+            // asserts
+            Check.That(response.StatusCode).IsEqualTo(HttpStatusCode.OK);
+            Check.That(content).IsEqualTo(JsonSerializer.Serialize(new { message = "Hello World !" }));
+
+            // dispose
+            await server.StopAsync();
+            PortManager.ReleasePort(server.Port);
+        }
+
+        [Fact]
+        public async Task Response_404_NotFound() {
+
+            // server
+            var server = new SimpleWServer(IPAddress.Loopback, PortManager.GetFreePort());
+            server.MapGet("/", (HttpSession session) => {
+                return session.Response.NotFound("NotFound");
+            });
+            await server.StartAsync();
+
+            // client
+            var client = new HttpClient();
+            var response = await client.GetAsync($"http://{server.Address}:{server.Port}/");
+            var content = await response.Content.ReadAsStringAsync();
+
+            // asserts
+            Check.That(response.StatusCode).IsEqualTo(HttpStatusCode.NotFound);
+            Check.That(content).IsEqualTo("NotFound");
+
+            // dispose
+            await server.StopAsync();
+            PortManager.ReleasePort(server.Port);
+        }
+
+        [Fact]
+        public async Task Response_500_ServerError() {
+
+            // server
+            var server = new SimpleWServer(IPAddress.Loopback, PortManager.GetFreePort());
+            server.MapGet("/", (HttpSession session) => {
+                return session.Response.InternalServerError("Server Error");
+            });
+            await server.StartAsync();
+
+            // client
+            var client = new HttpClient();
+            var response = await client.GetAsync($"http://{server.Address}:{server.Port}/");
+            var content = await response.Content.ReadAsStringAsync();
+
+            // asserts
+            Check.That(response.StatusCode).IsEqualTo(HttpStatusCode.InternalServerError);
+            Check.That(content).IsEqualTo("Server Error");
+
+            // dispose
+            await server.StopAsync();
+            PortManager.ReleasePort(server.Port);
+        }
+
+        [Fact]
+        public async Task Response_401_Unauthorized() {
+
+            // server
+            var server = new SimpleWServer(IPAddress.Loopback, PortManager.GetFreePort());
+            server.MapGet("/", (HttpSession session) => {
+                return session.Response.Unauthorized("Unauthorized");
+            });
+            await server.StartAsync();
+
+            // client
+            var client = new HttpClient();
+            var response = await client.GetAsync($"http://{server.Address}:{server.Port}/");
+            var content = await response.Content.ReadAsStringAsync();
+
+            // asserts
+            Check.That(response.StatusCode).IsEqualTo(HttpStatusCode.Unauthorized);
+            Check.That(content).IsEqualTo("Unauthorized");
+
+            // dispose
+            await server.StopAsync();
+            PortManager.ReleasePort(server.Port);
+        }
+
+        [Fact]
+        public async Task Response_302_Redirect() {
+
+            // server
+            var server = new SimpleWServer(IPAddress.Loopback, PortManager.GetFreePort());
+            server.MapGet("/", (HttpSession session) => {
+                return session.Response.Redirect($"http://{server.Address}:{server.Port}/");
+            });
+            await server.StartAsync();
+
+            // client
+            var client = new HttpClient();
+            var response = await client.GetAsync($"http://{server.Address}:{server.Port}/");
+            var content = await response.Content.ReadAsStringAsync();
+
+            // asserts
+            Check.That(response.StatusCode).IsEqualTo(HttpStatusCode.Redirect);
+
+            // dispose
+            await server.StopAsync();
+            PortManager.ReleasePort(server.Port);
+        }
+
+        [Fact]
+        public async Task Response_401_Access() {
+
+            // server
+            var server = new SimpleWServer(IPAddress.Loopback, PortManager.GetFreePort());
+            server.MapGet("/", (HttpSession session) => {
+                return session.Response.Access();
+            });
+            await server.StartAsync();
+
+            // client
+            var client = new HttpClient();
+            var response = await client.GetAsync($"http://{server.Address}:{server.Port}/");
+            var content = await response.Content.ReadAsStringAsync();
+
+            // asserts
+            Check.That(response.StatusCode).IsEqualTo(HttpStatusCode.Unauthorized);
+
+            // dispose
+            await server.StopAsync();
+            PortManager.ReleasePort(server.Port);
+        }
+
+        [Fact]
+        public async Task Response_403_Access() {
+
+            // server
+            var server = new SimpleWServer(IPAddress.Loopback, PortManager.GetFreePort());
+            server.MapGet("/", (HttpSession session) => {
+                //session.webuser = new WebUser() { Identity = true };
+                return session.Response.Access(true);
+            });
+            await server.StartAsync();
+
+            // client
+            var client = new HttpClient();
+            var response = await client.GetAsync($"http://{server.Address}:{server.Port}/");
+            var content = await response.Content.ReadAsStringAsync();
+
+            // asserts
+            Check.That(response.StatusCode).IsEqualTo(HttpStatusCode.Forbidden);
+
+            // dispose
+            await server.StopAsync();
+            PortManager.ReleasePort(server.Port);
+        }
+
+    }
+
+}
---
+++ src/test.rewrite/RouterContentTests.cs
@@ -0,0 +1,270 @@
+﻿using System.Net;
+using System.Text.Json;
+using NFluent;
+using SimpleW;
+using Xunit;
+
+
+namespace test {
+
+    /// <summary>
+    /// Tests for Router
+    /// </summary>
+    public class RouterTests {
+
+        #region dynamicmethod
+
+        [Fact]
+        public async Task Route_DynamicMethod_200() {
+
+            // server
+            var server = new SimpleWServer(IPAddress.Loopback, PortManager.GetFreePort());
+
+            server.UseController<Route_DynamicMethod_200_Controller>("/api");
+
+            await server.StartAsync();
+
+            // client
+            var client = new HttpClient();
+            var response = await client.GetAsync($"http://{server.Address}:{server.Port}/api/hello");
+            var content = await response.Content.ReadAsStringAsync();
+
+            // asserts
+            Check.That(response.StatusCode).Is(HttpStatusCode.OK);
+            Check.That(content).IsEqualTo(JsonSerializer.Serialize(new { message = "Hello World !" }));
+
+            // dispose
+            await server.StopAsync();
+            PortManager.ReleasePort(server.Port);
+        }
+
+        public class Route_DynamicMethod_200_Controller : Controller {
+            [Route("GET", "/hello")]
+            public object HelloWorld() {
+                return new { message = "Hello World !" };
+            }
+        }
+
+        #endregion dynamicmethod
+
+        #region dynamicmethodcontroller
+
+        [Fact]
+        public async Task Route_DynamicControllerMethod_200() {
+
+            // server
+            var server = new SimpleWServer(IPAddress.Loopback, PortManager.GetFreePort());
+
+            server.UseController<Route_DynamicControllerMethod_200_Controller>("/api");
+
+            await server.StartAsync();
+
+            // client
+            var client = new HttpClient();
+            var response = await client.GetAsync($"http://{server.Address}:{server.Port}/api/test/hello");
+            var content = await response.Content.ReadAsStringAsync();
+
+            // asserts
+            Check.That(response.StatusCode).Is(HttpStatusCode.OK);
+            Check.That(content).IsEqualTo(JsonSerializer.Serialize(new { message = "Hello World !" }));
+
+            // dispose
+            await server.StopAsync();
+            PortManager.ReleasePort(server.Port);
+        }
+
+        [Route("/test")]
+        public class Route_DynamicControllerMethod_200_Controller : Controller {
+            [Route("GET", "/hello")]
+            public object HelloWorld() {
+                return new { message = "Hello World !" };
+            }
+        }
+
+        #endregion dynamicmethodcontroller
+
+        #region dynamicmethodcontrollerabsolute
+
+        [Fact]
+        public async Task Route_DynamicControllerMethodAbsolute_404() {
+
+            // server
+            var server = new SimpleWServer(IPAddress.Loopback, PortManager.GetFreePort());
+
+            server.UseController<Route_DynamicControllerMethodAbsolute_Controller>("/api");
+
+            await server.StartAsync();
+
+            // client
+            var client = new HttpClient();
+            var response = await client.GetAsync($"http://{server.Address}:{server.Port}/api/test/hello");
+            var content = await response.Content.ReadAsStringAsync();
+
+            // asserts
+            Check.That(response.StatusCode).Is(HttpStatusCode.NotFound);
+
+            // dispose
+            await server.StopAsync();
+            PortManager.ReleasePort(server.Port);
+        }
+
+        [Fact]
+        public async Task Route_DynamicControllerMethodAbsolute_200() {
+
+            // server
+            var server = new SimpleWServer(IPAddress.Loopback, PortManager.GetFreePort());
+
+            server.UseController<Route_DynamicControllerMethodAbsolute_Controller>("/api");
+
+            await server.StartAsync();
+
+            // client
+            var client = new HttpClient();
+            var response = await client.GetAsync($"http://{server.Address}:{server.Port}/hello");
+            var content = await response.Content.ReadAsStringAsync();
+
+            // asserts
+            Check.That(response.StatusCode).Is(HttpStatusCode.OK);
+            Check.That(content).IsEqualTo(JsonSerializer.Serialize(new { message = "Hello World !" }));
+
+            // dispose
+            await server.StopAsync();
+            PortManager.ReleasePort(server.Port);
+        }
+
+        [Route("/test")]
+        public class Route_DynamicControllerMethodAbsolute_Controller : Controller {
+            [Route("GET", "/hello", isAbsolutePath: true)]
+            public object HelloWorld() {
+                return new { message = "Hello World !" };
+            }
+        }
+
+        #endregion dynamicmethodcontrollerabsolute
+
+        #region dynamic_catchall
+
+        [Fact]
+        public async Task Route_Dynamic_CatchAll_200() {
+
+            // server
+            var server = new SimpleWServer(IPAddress.Loopback, PortManager.GetFreePort());
+
+            server.UseController<Route_Dynamic_CatchAll_Controller>("/api");
+
+            await server.StartAsync();
+
+            // client
+            var client = new HttpClient();
+            var response = await client.GetAsync($"http://{server.Address}:{server.Port}/api/helloworld");
+            var content = await response.Content.ReadAsStringAsync();
+
+            // asserts
+            Check.That(response.StatusCode).Is(HttpStatusCode.OK);
+            Check.That(content).IsEqualTo(JsonSerializer.Serialize(new { message = "Hello World !" }));
+
+            // dispose
+            await server.StopAsync();
+            PortManager.ReleasePort(server.Port);
+        }
+
+        public class Route_Dynamic_CatchAll_Controller : Controller {
+            [Route("GET", "/*")]
+            public object HelloWorld() {
+                return new { message = "Hello World !" };
+            }
+        }
+
+        #endregion dynamic_catchall
+
+        #region onbeforemethod
+
+        [Fact]
+        public async Task OnBeforeController_200() {
+
+            // server
+            var server = new SimpleWServer(IPAddress.Loopback, PortManager.GetFreePort());
+
+            server.UseController<OnBeforeController>("/api");
+
+            await server.StartAsync();
+
+            // client
+            var client = new HttpClient();
+            var response = await client.GetAsync($"http://{server.Address}:{server.Port}/api/test/hello");
+            var content = await response.Content.ReadAsStringAsync();
+
+            // asserts
+            Check.That(response.StatusCode).Is(HttpStatusCode.OK);
+            Check.That(content).IsEqualTo(JsonSerializer.Serialize(new { message = "Chris, Hello World !" }));
+
+            // dispose
+            await server.StopAsync();
+            PortManager.ReleasePort(server.Port);
+        }
+
+        [Route("/test")]
+        public class OnBeforeController : Controller {
+
+            private string Name = "";
+
+            public override void OnBeforeMethod() {
+                Name = "Chris";
+            }
+
+            [Route("GET", "/hello")]
+            public object HelloWorld() {
+                return new { message = $"{Name}, Hello World !" };
+            }
+
+        }
+
+        #endregion onbeforemethod
+
+        #region subclass
+
+        [Fact]
+        public async Task Subclass_200() {
+
+            // server
+            var server = new SimpleWServer(IPAddress.Loopback, PortManager.GetFreePort());
+
+            server.UseController<SubclassController>("/api");
+
+            await server.StartAsync();
+
+            // client
+            var client = new HttpClient();
+            var response = await client.GetAsync($"http://{server.Address}:{server.Port}/api/test/hello");
+            var content = await response.Content.ReadAsStringAsync();
+
+            // asserts
+            Check.That(response.StatusCode).Is(HttpStatusCode.OK);
+            Check.That(content).IsEqualTo(JsonSerializer.Serialize(new { message = "Chris, Hello World !" }));
+
+            // dispose
+            await server.StopAsync();
+            PortManager.ReleasePort(server.Port);
+        }
+
+        public class Subclass : Controller {
+
+            protected string Name = "Chris";
+
+        }
+
+        [Route("/test")]
+        public class SubclassController : Subclass {
+
+            [Route("GET", "/hello")]
+            public object HelloWorld() {
+                return new { message = $"{Name}, Hello World !" };
+            }
+
+        }
+
+        #endregion subclass
+
+    }
+
+}
