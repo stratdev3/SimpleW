2671265ecf7345658bb1f253901f097732b70076
Christophe CHATEAU <c.chateau@stratdev.fr>
2025-12-23 19:51:45 +0100

    refactor(REWRITE): comment

--- src/SimpleW/buffers/BufferWriterStream.cs
+++ src/SimpleW/buffers/BufferWriterStream.cs
@@ -8,26 +8,94 @@ namespace SimpleW.Buffers {
     /// </summary>
     internal sealed class BufferWriterStream : Stream {
 
+        /// <summary>
+        /// The underlying writer
+        /// </summary>
         private readonly IBufferWriter<byte> _writer;
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="writer"></param>
         public BufferWriterStream(IBufferWriter<byte> writer) => _writer = writer;
 
+        /// <summary>
+        /// Can read
+        /// </summary>
         public override bool CanRead => false;
+
+        /// <summary>
+        /// Can seek
+        /// </summary>
         public override bool CanSeek => false;
+
+        /// <summary>
+        /// Can write
+        /// </summary>
         public override bool CanWrite => true;
+
+        /// <summary>
+        /// Length
+        /// </summary>
         public override long Length => throw new NotSupportedException();
+
+        /// <summary>
+        /// Seek Position
+        /// </summary>
         public override long Position { get => throw new NotSupportedException(); set => throw new NotSupportedException(); }
 
+        /// <summary>
+        /// Flush
+        /// </summary>
         public override void Flush() { }
+
+        /// <summary>
+        /// FlushAsync
+        /// </summary>
+        /// <param name="cancellationToken"></param>
+        /// <returns></returns>
         public override Task FlushAsync(CancellationToken cancellationToken) => Task.CompletedTask;
 
+        /// <summary>
+        /// Read
+        /// </summary>
+        /// <param name="buffer"></param>
+        /// <param name="offset"></param>
+        /// <param name="count"></param>
+        /// <returns></returns>
+        /// <exception cref="NotSupportedException"></exception>
         public override int Read(byte[] buffer, int offset, int count) => throw new NotSupportedException();
+
+        /// <summary>
+        /// Seek
+        /// </summary>
+        /// <param name="offset"></param>
+        /// <param name="origin"></param>
+        /// <returns></returns>
+        /// <exception cref="NotSupportedException"></exception>
         public override long Seek(long offset, SeekOrigin origin) => throw new NotSupportedException();
+
+        /// <summary>
+        /// SetLength
+        /// </summary>
+        /// <param name="value"></param>
+        /// <exception cref="NotSupportedException"></exception>
         public override void SetLength(long value) => throw new NotSupportedException();
 
+        /// <summary>
+        /// Write
+        /// </summary>
+        /// <param name="buffer"></param>
+        /// <param name="offset"></param>
+        /// <param name="count"></param>
         public override void Write(byte[] buffer, int offset, int count) {
             Write(buffer.AsSpan(offset, count));
         }
 
+        /// <summary>
+        /// Write
+        /// </summary>
+        /// <param name="buffer"></param>
         public override void Write(ReadOnlySpan<byte> buffer) {
             if (buffer.IsEmpty)
                 return;
@@ -36,11 +104,25 @@ namespace SimpleW.Buffers {
             _writer.Advance(buffer.Length);
         }
 
+        /// <summary>
+        /// WriteAsync
+        /// </summary>
+        /// <param name="buffer"></param>
+        /// <param name="cancellationToken"></param>
+        /// <returns></returns>
         public override ValueTask WriteAsync(ReadOnlyMemory<byte> buffer, CancellationToken cancellationToken = default) {
             Write(buffer.Span);
             return default;
         }
 
+        /// <summary>
+        /// WriteAsync
+        /// </summary>
+        /// <param name="buffer"></param>
+        /// <param name="offset"></param>
+        /// <param name="count"></param>
+        /// <param name="cancellationToken"></param>
+        /// <returns></returns>
         public override Task WriteAsync(byte[] buffer, int offset, int count, CancellationToken cancellationToken) {
             Write(buffer.AsSpan(offset, count));
             return Task.CompletedTask;
