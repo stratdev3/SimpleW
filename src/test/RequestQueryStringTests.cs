0a0dd3d3b9d3d6e464a4b72a5fe12633221cd76f
Christophe CHATEAU <c.chateau@stratdev.fr>
2026-01-11 23:32:13 +0100

    feature(test): add test with qs parameter Guid for map and controller (#201)

---
+++ src/test/RequestQueryStringTests.cs
@@ -0,0 +1,246 @@
+﻿using System.Net;
+using System.Text.Json;
+using System.Xml.Linq;
+using NFluent;
+using SimpleW;
+using Xunit;
+
+
+namespace test {
+
+    /// <summary>
+    /// Tests for QueryString
+    /// </summary>
+    public class RequestQueryStringTests {
+
+        #region no default
+
+        [Fact]
+        public async Task QueryString_MapGet_String_NoDefault_200() {
+
+            // server
+            var server = new SimpleWServer(IPAddress.Loopback, PortManager.GetFreePort());
+            server.MapGet("/api/test/hello", (HttpSession session, string name) => {
+                return new { message = $"{name}, Hello World !" };
+            });
+            await server.StartAsync();
+
+            // client
+            var client = new HttpClient();
+            var response = await client.GetAsync($"http://{server.Address}:{server.Port}/api/test/hello?name=Chris");
+            var content = await response.Content.ReadAsStringAsync();
+
+            // asserts
+            Check.That(response.StatusCode).Is(HttpStatusCode.OK);
+            Check.That(content).IsEqualTo(JsonSerializer.Serialize(new { message = "Chris, Hello World !" }));
+
+            // dispose
+            await server.StopAsync();
+            PortManager.ReleasePort(server.Port);
+        }
+
+        [Fact]
+        public async Task QueryString_MapGet_String_NoDefault_500() {
+
+            // server
+            var server = new SimpleWServer(IPAddress.Loopback, PortManager.GetFreePort());
+            server.MapGet("/api/test/hello", (HttpSession session, string name) => {
+                return new { message = $"{name}, Hello World !" };
+            });
+            await server.StartAsync();
+
+            // client
+            var client = new HttpClient();
+            var response = await client.GetAsync($"http://{server.Address}:{server.Port}/api/test/hello");
+            var content = await response.Content.ReadAsStringAsync();
+
+            // asserts
+            Check.That(response.StatusCode).Is(HttpStatusCode.InternalServerError);
+
+            // dispose
+            await server.StopAsync();
+            PortManager.ReleasePort(server.Port);
+        }
+
+        [Fact]
+        public async Task QueryString_MapGet_Guid_NoDefault_200() {
+
+            // server
+            var server = new SimpleWServer(IPAddress.Loopback, PortManager.GetFreePort());
+            server.MapGet("/api/user/update", (HttpSession session, Guid id) => {
+                return new { message = $"{id}" };
+            });
+            await server.StartAsync();
+
+            // client
+            var client = new HttpClient();
+            var id = Guid.NewGuid();
+            var response = await client.GetAsync($"http://{server.Address}:{server.Port}/api/user/update?id={id}");
+            var content = await response.Content.ReadAsStringAsync();
+
+            // asserts
+            Check.That(response.StatusCode).Is(HttpStatusCode.OK);
+            Check.That(content).IsEqualTo(JsonSerializer.Serialize(new { message = id }));
+
+            // dispose
+            await server.StopAsync();
+            PortManager.ReleasePort(server.Port);
+        }
+
+        [Fact]
+        public async Task QueryString_MapGet_Guid_NoDefault_500() {
+
+            // server
+            var server = new SimpleWServer(IPAddress.Loopback, PortManager.GetFreePort());
+            server.MapGet("/api/user/update", (HttpSession session, Guid id) => {
+                return new { message = $"{id}" };
+            });
+            await server.StartAsync();
+
+            // client
+            var client = new HttpClient();
+            var response = await client.GetAsync($"http://{server.Address}:{server.Port}/api/user/update");
+            var content = await response.Content.ReadAsStringAsync();
+
+            // asserts
+            Check.That(response.StatusCode).Is(HttpStatusCode.InternalServerError);
+
+            // dispose
+            await server.StopAsync();
+            PortManager.ReleasePort(server.Port);
+        }
+
+        #endregion no default
+
+        #region default value
+
+        [Fact]
+        public async Task QueryString_MapGet_String_DefaultNull_GetNull_200() {
+
+            // server
+            var server = new SimpleWServer(IPAddress.Loopback, PortManager.GetFreePort());
+            server.MapGet("/api/test/hello", (HttpSession session, string? name = null) => {
+                return new { message = $"{name}, Hello World !" };
+            });
+            await server.StartAsync();
+
+            // client
+            var client = new HttpClient();
+            var response = await client.GetAsync($"http://{server.Address}:{server.Port}/api/test/hello");
+            var content = await response.Content.ReadAsStringAsync();
+
+            // asserts
+            Check.That(response.StatusCode).Is(HttpStatusCode.OK);
+            Check.That(content).IsEqualTo(JsonSerializer.Serialize(new { message = ", Hello World !" }));
+
+            // dispose
+            await server.StopAsync();
+            PortManager.ReleasePort(server.Port);
+        }
+
+        [Fact]
+        public async Task QueryString_MapGet_String_DefaultNull_GetEmpty_200() {
+
+            // server
+            var server = new SimpleWServer(IPAddress.Loopback, PortManager.GetFreePort());
+            server.MapGet("/api/test/hello", (HttpSession session, string? name = null) => {
+                return new { message = $"{name}, Hello World !" };
+            });
+            await server.StartAsync();
+
+            // client
+            var client = new HttpClient();
+            var response = await client.GetAsync($"http://{server.Address}:{server.Port}/api/test/hello?name=");
+            var content = await response.Content.ReadAsStringAsync();
+
+            // asserts
+            Check.That(response.StatusCode).Is(HttpStatusCode.OK);
+            Check.That(content).IsEqualTo(JsonSerializer.Serialize(new { message = ", Hello World !" }));
+
+            // dispose
+            await server.StopAsync();
+            PortManager.ReleasePort(server.Port);
+        }
+
+        [Fact]
+        public async Task QueryString_MapGet_String_DefaultNull_GetValue_200() {
+
+            // server
+            var server = new SimpleWServer(IPAddress.Loopback, PortManager.GetFreePort());
+            server.MapGet("/api/test/hello", (HttpSession session, string? name = null) => {
+                return new { message = $"{name}, Hello World !" };
+            });
+            await server.StartAsync();
+
+            // client
+            var client = new HttpClient();
+            var response = await client.GetAsync($"http://{server.Address}:{server.Port}/api/test/hello?name=Chris");
+            var content = await response.Content.ReadAsStringAsync();
+
+            // asserts
+            Check.That(response.StatusCode).Is(HttpStatusCode.OK);
+            Check.That(content).IsEqualTo(JsonSerializer.Serialize(new { message = "Chris, Hello World !" }));
+
+            // dispose
+            await server.StopAsync();
+            PortManager.ReleasePort(server.Port);
+        }
+
+        #endregion default value
+
+        #region default object
+
+        [Fact]
+        public async Task QueryString_MapGet_Guid_DefaultObject_GetNull_200() {
+
+            // server
+            var server = new SimpleWServer(IPAddress.Loopback, PortManager.GetFreePort());
+            server.MapGet("/api/user/update", (HttpSession session, Guid id = new Guid()) => {
+                return new { message = $"{id}" };
+            });
+            await server.StartAsync();
+
+            // client
+            var client = new HttpClient();
+            var response = await client.GetAsync($"http://{server.Address}:{server.Port}/api/user/update");
+            var content = await response.Content.ReadAsStringAsync();
+
+            // asserts
+            Check.That(response.StatusCode).Is(HttpStatusCode.OK);
+            Check.That(content).IsEqualTo(JsonSerializer.Serialize(new { message = Guid.Empty }));
+
+            // dispose
+            await server.StopAsync();
+            PortManager.ReleasePort(server.Port);
+        }
+
+        [Fact]
+        public async Task QueryString_MapGet_Guid_DefaultObject_GetValue_200() {
+
+            // server
+            var server = new SimpleWServer(IPAddress.Loopback, PortManager.GetFreePort());
+            server.MapGet("/api/user/update", (HttpSession session, Guid id = new Guid()) => {
+                return new { message = $"{id}" };
+            });
+            await server.StartAsync();
+
+            // client
+            var client = new HttpClient();
+            var id = Guid.NewGuid();
+            var response = await client.GetAsync($"http://{server.Address}:{server.Port}/api/user/update?id={id}");
+            var content = await response.Content.ReadAsStringAsync();
+
+            // asserts
+            Check.That(response.StatusCode).Is(HttpStatusCode.OK);
+            Check.That(content).IsEqualTo(JsonSerializer.Serialize(new { message = id }));
+
+            // dispose
+            await server.StopAsync();
+            PortManager.ReleasePort(server.Port);
+        }
+
+        #endregion default object
+
+    }
+
+}
