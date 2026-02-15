5b4b05ec353b0847c675237a5c4ec6d8d23fa9e4
Christophe CHATEAU <c.chateau@stratdev.fr>
2026-02-15 17:37:43 +0100

    feature(REWRITE): new package SimpleW.Templates (#288)

---
+++ src/SimpleW.Templates/readme.md
@@ -0,0 +1,30 @@
+# SimpleW.Templates
+
+[![website](https://raw.githubusercontent.com/stratdev3/SimpleW/refs/heads/master/documentation/simplew/docs/public/simplew-og.png)](https://simplew.net)
+
+[![NuGet Package](https://img.shields.io/nuget/v/SimpleW)](https://www.nuget.org/packages/SimpleW)
+![NuGet Downloads](https://img.shields.io/nuget/dt/SimpleW)
+[![License](https://img.shields.io/badge/License-MIT-green.svg)](licence)
+<br/>
+[![Linux](https://github.com/stratdev3/SimpleW/actions/workflows/build-linux.yml/badge.svg)](https://github.com/stratdev3/SimpleW/actions/workflows/build-linux.yml)
+[![MacOS](https://github.com/stratdev3/SimpleW/actions/workflows/build-macos.yml/badge.svg)](https://github.com/stratdev3/SimpleW/actions/workflows/build-macos.yml)
+[![Windows (Visual Studio)](https://github.com/stratdev3/SimpleW/actions/workflows/build-windows.yml/badge.svg)](https://github.com/stratdev3/SimpleW/actions/workflows/build-windows.yml)
+
+### Features
+
+Dotnet templates based on SimpleW.
+
+## Documentation
+
+To check out docs, visit [simplew.net](https://simplew.net).
+
+## Changelog
+
+Detailed changes for each release are documented in the [CHANGELOG](https://github.com/stratdev3/SimpleW/blob/master/release.md).
+
+## Contribution
+
+Feel free to report issue.
+
+## License
+This library is under the MIT License.
---
+++ src/SimpleW.Templates/templates/SimpleW.Minimal/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Diagnostics;
+using System.Net;
+using System.Threading.Tasks;
+using SimpleW;
+
+
+namespace ModuleName {
+
+    internal class Program {
+
+        /// <summary>
+        /// EntryPoint
+        /// </summary>
+        /// <param name="args"></param>
+        static async Task Main(string[] args) {
+
+            // server
+            SimpleWServer server = new(IPAddress.Any, 2015);
+
+            server.Configure(options => {
+                // network options for better performances
+                options.ReuseAddress = true;
+                options.TcpNoDelay = true;
+                options.TcpKeepAlive = true;
+                options.AcceptPerCore = true;
+            });
+
+            // default handler
+            server.MapGet("/", () => {
+                return new { message = "Hello World !" };
+            });
+
+            // subscribe to events
+            server.OnStarted += (object? sender, EventArgs e) => {
+                string url = $"http://localhost:{server.Port}";
+                Console.WriteLine($"server started at {url}");
+                Process.Start(new ProcessStartInfo {
+                    FileName = url,
+                    UseShellExecute = true
+                });
+            };
+            server.OnStopped += (object? sender, EventArgs e) => {
+                Console.WriteLine("server stopped");
+            };
+
+            // run and block
+            await server.RunAsync();
+        }
+
+    }
+
+}
