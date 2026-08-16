366cc64f686d8357b9208b7b2797911a17d1c34a
Christophe CHATEAU <c.chateau@stratdev.fr>
2026-08-16 02:32:54 +0200

    refactor(documentation): bump

--- documentation/simplew/docs/.vitepress/config.mjs
+++ documentation/simplew/docs/.vitepress/config.mjs
@@ -40,7 +40,7 @@ export default defineConfig({
             },
             //{ text: '📖✨ Features', link: '/features' },
             {
-                text: 'v26.0.1',
+                text: 'v26.1.0',
                 items: [
                     { text: 'Changelog', link: '/version/changelog' },
                     { text: 'FAQ', link: '/version/faq' },
