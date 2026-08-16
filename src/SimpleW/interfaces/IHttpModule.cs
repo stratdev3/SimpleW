32f789054a720fb73943216d9056c9e47b6c3a02
Christophe CHATEAU <c.chateau@stratdev.fr>
2026-08-17 00:19:56 +0200

    refactor(SimpleW): comment

--- src/SimpleW/interfaces/IHttpModule.cs
+++ src/SimpleW/interfaces/IHttpModule.cs
@@ -6,7 +6,7 @@
     public interface IHttpModule {
 
         /// <summary>
-        /// Callback to Install Module in server
+        /// Callback invoked by <see cref="SimpleWServer.UseModule"/> to install the module.
         /// </summary>
         /// <param name="server"></param>
         void Install(SimpleWServer server);
